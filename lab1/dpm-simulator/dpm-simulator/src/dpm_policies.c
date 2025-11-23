a72a4bd3378e7846f30b9d552c953d17f5572594
ZHOU XUXIN s337564 <s337564@studenti.polito.it>
2025-11-23 12:25:42 +0100

    updated:newpolicy for part3

--- lab1/dpm-simulator/dpm-simulator/src/dpm_policies.c
+++ lab1/dpm-simulator/dpm-simulator/src/dpm_policies.c
@@ -175,8 +175,9 @@ int dpm_decide_state(psm_state_t *next_state, psm_state_t prev_state, psm_time_t
             }
             break;
 	*/
+	/*
 	case DPM_HISTORY:
-            /* Improved Hybrid Policy  result for workload2 good improve, for workload1 close to best result of timeout policy*/
+            //Improved Hybrid Policy  result for workload2 good improve, for workload1 close to best result of timeout policy
             
             // 1. 获取预测值 (Last Value Prediction)
             psm_time_t predicted_time = history[DPM_HIST_WIND_SIZE - 1];
@@ -204,6 +205,32 @@ int dpm_decide_state(psm_state_t *next_state, psm_state_t prev_state, psm_time_t
                 } else {
                     // 还没到 1ms，先在 RUN 等一等，避免极其频繁的微小抖动
                     *next_state = PSM_STATE_RUN;
+                }
+            }
+            break;
+            */
+            case DPM_HISTORY:
+            // Part 3: Advanced Hybrid Policy (Inspired by Part 2 Results) 
+            
+            psm_time_t predicted_time = history[DPM_HIST_WIND_SIZE - 1];
+            double t_be_sleep = 75.0; 
+            
+            // 策略 A: 确信是长空闲 (Workload 2 模式)
+            // 预测值很大，说明我们在稳定的长休眠周期里 -> 立即睡，零等待
+            if (predicted_time >= t_be_sleep) {
+                *next_state = PSM_STATE_SLEEP;
+            }
+            
+            // 策略 B: 预测是短空闲 (Workload 1 模式)
+            // 我们不确定这次会不会变长，所以采取“保守的 Sleep 策略” (Timeout to Sleep)
+            // 参考 Part 2 的实验数据，Workload 1 的最佳超时时间是 4ms
+            else {
+                double smart_timeout = 4.0; // Magic Number form Part 2 results
+                
+                if (t_curr > t_inactive_start + smart_timeout) {
+                    *next_state = PSM_STATE_SLEEP; // 超过 4ms 还没任务，说明可能是长空闲，切 Sleep！
+                } else {
+                    *next_state = PSM_STATE_RUN;   // 还没到 4ms，先观望，避免误切
                 }
             }
             break;
